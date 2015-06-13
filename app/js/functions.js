8f016613202323e6b6f821345079c13ce1136fb5
MarioGranada <mario.granada190@gmail.com>
2015-06-13 18:51:23 -0500

    new_updates2

--- app/js/functions.js
+++ app/js/functions.js
@@ -12,13 +12,19 @@ function openAlbum(element){
 	$(element).siblings(".image-box-container").toggleClass("image-box-container-shown")
 	return false;
 }
-function openImagePopup(element){
+function openPopup(element){
 	$(".popup-overlay").toggleClass("popup-overlay-open");
-	$(element).siblings(".image-window").toggleClass("image-window-open")
+	$(".car-popup-title").html($(element).siblings(".car-full-title").html());
+	$(".car-details-image").html($(element).siblings(".main-car-img").html());
+	$(".car-details").html($(element).siblings(".car-basic-info-box").html())
+	$(".car-details").append($(element).siblings(".car-main-info"));
+	$(".car-popup").fadeIn(500);
+	// $(window).animate({scrollTop:0}, '500', 'swing');
+	$("html, body").animate({ scrollTop: "0px" });
 	return false;
 }
 function closePopup(){
+	$(".car-popup").fadeOut(500);
 	$(".popup-overlay").toggleClass("popup-overlay-open");
-	$(".image-window-open").toggleClass("image-window-open");
 	return false;
 }
